83ffbc89ba0149114797a0bf0bb8d2ee6168e4f6
Tycon410 <Tycon410@outlook.com>
2026-07-22 20:53:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Better Recycling
+
+
+
+## License
+
+This mod is available under the MIT license. Feel free to use it.
---
+++ src/client/java/com/tycon/better_recycling/client/BetterRecyclingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tycon.better_recycling.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterRecyclingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
